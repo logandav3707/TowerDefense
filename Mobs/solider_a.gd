f62c46e104e127d0a7c6a8cf48abc1fc4478f254
logandav3707 <ldavis@ftstudent.org>
2024-09-23 13:12:55 -0400

    f

--- Mobs/solider_a.gd
+++ Mobs/solider_a.gd
@@ -6,4 +6,4 @@ extends CharacterBody2D
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
-		queue_free() 
+		queue_free()
--- scene/path_spawner.gd
+++ scene/path_spawner.gd
@@ -1,8 +1,4 @@
 extends Node2D
 
-@onready var path = preload("res://scene/Stage 1.tscn")
 
-func _on_timer_timeout() -> void:
-	var tempPath = path.instantiate()
-	add_child(tempPath)
 
