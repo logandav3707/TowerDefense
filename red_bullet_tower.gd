0aad611d6a99d76d2b37a51e4910176054f7de5e
logandav3707 <ldavis@ftstudent.org>
2024-09-30 13:16:42 -0400

    vc

--- red_bullet_tower.gd
+++ red_bullet_tower.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
 var Bullet = preload("res://Towers/RedBullet.gd")
-var BulletDamage = 5
+var bulletDamage = 5
 var pathName
 var currTargets = []
 var curr
@@ -25,5 +25,15 @@ func _on_tower_body_entered(body:Node2D) -> void:
 				if i.get_parent().get_progress() > currTarget.get_progress():
 					currTarget = i.get_node("../")
 
-func _on_tower_body_exited(body:Node2D) -> void:
+		curr = currTarget
+		pathName = currTarget.get_parent().name
+
+		var tempBullet = Bullet.instantiate()
+		tempBullet.pathName = pathName
+		tempBullet.bulletDamage = bulletDamage
+		get_node("BulletContainer").add_child(tempBullet)
+		tempBullet.global_position = $Aim.global_position
+
+
+func _on_tower_body_exited(_body:Node2D) -> void:
 	pass
